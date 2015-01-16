0c0435763116d417b86df39643ad52d9c78577fd
boydsleep <jkates@boydspecialtysleep.com>
2015-01-16 10:05:46 -0600

    V1

---
+++ src/Box.java
@@ -0,0 +1,10 @@
+/**
+ * Created by jeremykates on 1/15/15.
+ */
+public class Box {
+
+    double width;
+    double height;
+    double depth;
+
+}
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+        String superSede = "jeff davis";
+
+        System.out.println(superSede);
+
+        Box myBox = new Box();
+
+        myBox.depth = 25;
+
+        System.out.println(myBox.depth);
+
+    }
+
+}
